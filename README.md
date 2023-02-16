ba63e537c392f22f57721275c45cd6d168e450f5
eethan883 <eethan883@gmail.com>
2023-02-16 16:01:08 +0800

    Demo Controller

---
+++ README.md
@@ -0,0 +1,3 @@
+# 秒杀项目
+[视频链接](www.bilibili.com/video/BV1sf4y1L7KE)
+
---
+++ src/main/java/com/lee/seckill/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.lee.seckill.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "xxx");
+        return "hello";
+    }
+}
